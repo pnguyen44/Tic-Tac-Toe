301e2c031b261dbbb2eb455342e81306f3906803
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-09-01 12:49:05 -0400

    Update config.js updated api-origins

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,8 @@
 const config = {
   apiOrigins: {
     // production: 'https://ga-wdi-boston.herokuapp.com'
-    production: 'https://aqueous-atoll-85096.herokuapp.com/'
+    production: 'https://aqueous-atoll-85096.herokuapp.com',
+    development: 'http://tic-tac-toe.wdibos.com'
   }
 }
 
