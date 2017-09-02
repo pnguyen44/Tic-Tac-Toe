173cf675e2b3fdfdda35368d0fc7fa93ae096875
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-09-01 21:28:20 -0400

    Update store.js removed some properties in store object

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,12 +1,6 @@
 'use strict'
 
 const store = {
-  cells: ['', '', '', '', '', '', '', '', ''],
-  clickCount: 0,
-  status: 'active',
-  winner: '',
-  player_x: 0,
-  player_o: 0
 }
 
 module.exports = store
