0b191e63d2021e50f4e8caa11407ce070f21e088
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-09-01 21:26:59 -0400

    Update game.js refactored code

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,6 +1,7 @@
+const gameApi = require('./games/api')
 const store = require('./store')
-let board = store.cells
-
+let cells = ['', '', '', '', '', '', '', '', '']
+store.over = true
 const isEmpty = function (element) {
   const len = element.html().length
   // console.log('length ', len)
@@ -12,18 +13,18 @@ const isEmpty = function (element) {
 }
 let clickCount = store.clickCount
 
-// add response if empty and add to board
+// add response if empty and add to cells
 const playGame = function (element) {
   const player = clickCount % 2 === 0 ? 'x' : 'o'
   // clickCount = countClicks()
   const empty = isEmpty(element)
-  if (store.status === 'active') {
+  if (store.over === 'active') {
     if (empty) {
       clickCount += 1
       console.log('click count', clickCount)
-      board[element.attr('id')] = player
+      cells[element.attr('id')] = player
       element.html(player)
-      const isWinner = checkForWinner(board, player)
+      const isWinner = checkForWinner(cells, player)
       console.log('isWinner ' + isWinner)
       if (isWinner) {
         console.log('Winner ' + player)
@@ -31,7 +32,7 @@ const playGame = function (element) {
         $('.btn-play').html('Winner is ' + player + '! Play again.')
         // $('.btn-play').off('click', '**')
         updateScore(player)
-        store.status = 'inactive'
+        store.over = false
         // return true
       } else if (clickCount === 9) {
         console.log('There is a tie.')
@@ -43,7 +44,7 @@ const playGame = function (element) {
 }
 
 const updateScore = function (player) {
-  let element = '#player_' + player + '_score'
+  const element = '#player_' + player + '_score'
   // console.log($(element))
   let score = parseInt($(element).html())
   if (score === 'NAN') {
@@ -55,18 +56,17 @@ const updateScore = function (player) {
   // console.log($('element').html(score + 1))
 }
 
-
-const checkForWinner = function (board, player) {
+const checkForWinner = function (cells, player) {
   const val = player
   // const winner = val
-  if (((board[0] === val) && (board[1] === val) && (board[2] === val)) ||
-    ((board[3] === val) && (board[4] === val) && (board[5] === val)) ||
-    ((board[6] === val) && (board[7] === val) && (board[8] === val)) ||
-    ((board[0] === val) && (board[3] === val) && (board[6] === val)) ||
-    ((board[1] === val) && (board[4] === val) && (board[7] === val)) ||
-    ((board[2] === val) && (board[5] === val) && (board[8] === val)) ||
-    ((board[0] === val) && (board[4] === val) && (board[8] === val)) ||
-    ((board[2] === val) && (board[4] === val) && (board[6] === val))) {
+  if (((cells[0] === val) && (cells[1] === val) && (cells[2] === val)) ||
+    ((cells[3] === val) && (cells[4] === val) && (cells[5] === val)) ||
+    ((cells[6] === val) && (cells[7] === val) && (cells[8] === val)) ||
+    ((cells[0] === val) && (cells[3] === val) && (cells[6] === val)) ||
+    ((cells[1] === val) && (cells[4] === val) && (cells[7] === val)) ||
+    ((cells[2] === val) && (cells[5] === val) && (cells[8] === val)) ||
+    ((cells[0] === val) && (cells[4] === val) && (cells[8] === val)) ||
+    ((cells[2] === val) && (cells[4] === val) && (cells[6] === val))) {
     return true
   } else {
     return false
@@ -75,10 +75,11 @@ const checkForWinner = function (board, player) {
 
 const resetGame = function () {
   clickCount = 0
-  store.status = 'active'
+  store.over = 'active'
   $('.box').text('')
-  board = ['', '', '', '', '', '', '', '', '']
+  cells = ['', '', '', '', '', '', '', '', '']
   $(this).html('Play New Game')
+  // store.game = gameApi.create
 }
 
 module.exports = {
