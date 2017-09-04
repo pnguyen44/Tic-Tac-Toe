29b173fab632c9385185f70b070f64f999f2f3ea
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-09-03 21:38:01 -0400

    Updated ui.js updated onSignUpSuccess function

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,10 +1,20 @@
 'use strict'
 const store = require('../store')
-
+const api = require('./api')
+const getFormFields = require('../../../lib/get-form-fields')
+const authEvents = require('./events')
+const gamesEvents = require('../games/events')
+const game = require('../game')
 const signUpSuccess = function (data) {
-  console.log(data)
+  console.log('data', data)
+  console.log('store.user', data.user)
   console.log('Successfully Signed Up')
+  store.user = data.user
+  // console.log('data.user', data.user)
+  // api.signIn(data)
+  // api.signIn(store.user)
   clearForm()
+  // api.signIn(data.user.id)
 }
 const signUpFailure = function (error) {
   console.log(error)
@@ -15,8 +25,15 @@ const signUpFailure = function (error) {
 const signInSuccess = function (data) {
   console.log('Successfully Signed In')
   store.user = data.user
-  console.log(store.user)
+  store.token = data.user.token
+  store.isSignedIn = true
+  $('#change-password').show()
+  $('#btn-sign-out').show()
+  $('#view-history').show()
+  $('#sign-up').hide()
   clearForm()
+  gamesEvents.getGames()
+  game.getPlayerStats()
 }
 const signInFailure = function (error) {
   console.error(error)
